5fc54cd1209f4582e8000675ede2278944265c6d
Matt Bomhoff <mbomhoff@email.arizona.edu>
2017-12-20 15:16:30 -0700

    Get running on latest node v9.3.0

--- bin/example.sh
+++ bin/example.sh
@@ -4,4 +4,4 @@ mkdir -p example/.build
 watchify example/example.js -o example/.build/example.js &
 watchify example/minimal/example.js -o example/.build/minimal.js &
 stylus src -o example/.build/ --watch &
-serve -L example
+serve example
