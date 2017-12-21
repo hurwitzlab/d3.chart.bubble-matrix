6c260fa091ec985a38bccd491907b8ddd9beefc7
Matt Bomhoff <mbomhoff@email.arizona.edu>
2017-12-21 12:27:44 -0700

    Update dist

--- dist/chart.js
+++ dist/chart.js
@@ -26740,7 +26740,10 @@ var Chart = d3.chart('BaseChart').extend(CHART_NAME, {
     },
 
     _getMaxBottom: function (data, height) {
-        return height - 2 * this._ruler.extentOfChar('W').height;
+        var maxLen = 2;
+        if (this._slanted)
+            maxLen = Math.max.apply(null, data.map(s => { return s.length }));
+        return height - maxLen * this._ruler.extentOfChar('W').height;
     },
 
     rows: makeProp('_rows'),
@@ -26892,6 +26895,9 @@ function transformCol(sel, chart) {
         }
         return result;
     });
+    if (slanted) {
+        sel.attr('text-anchor', 'start');
+    }
 }
 
 layer.events['enter'] = function () {
